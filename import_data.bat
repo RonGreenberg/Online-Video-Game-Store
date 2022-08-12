9549ab20fb7f9f3992b39191ce9bc033e2c1cefb
Ron Greenberg <ron.greenberg@cs.colman.ac.il>
2022-08-12 17:54:08 +0300

    Added games collection

--- import_data.bat
+++ import_data.bat
@@ -1,4 +1,5 @@
 start /min cmd /c mongod --dbpath="c:\data\db"
 mongoimport --db online-video-game-store --collection developers --file developers.json --jsonArray --mode=upsert
 mongoimport --db online-video-game-store --collection customers --file customers.json --jsonArray --mode=upsert
+mongoimport --db online-video-game-store --collection games --file games.json --jsonArray --mode=upsert
 taskkill /F /IM mongod.exe
