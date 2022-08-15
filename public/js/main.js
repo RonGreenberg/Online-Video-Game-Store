bc7242dd7ef4cfc420586d35e9776d2bd2e321d0
Yonatan Birman <jonatanbir10@gmail.com>
2022-08-15 17:45:58 +0300

    orders (still on the making)

--- public/js/main.js
+++ public/js/main.js
@@ -39,9 +39,28 @@ function selectPage(hashKey) {
         case "#games":
             renderGames();
             break;
+        case "#orders":
+            renderOrders();
+            break;
     }
 }
+function renderOrders(){
+    $('.table-expandable').each(function () {
+        var table = $(this);
+        table.children('thead').children('tr').append('<th></th>');
+        table.children('tbody').children('tr').filter(':odd').hide();
+        table.children('tbody').children('tr').filter(':even').click(function () {
+            var element = $(this);
+            element.next('tr').toggle('fast');
+            element.find(".table-expandable-arrow").toggleClass("up");
+        });
+        table.children('tbody').children('tr').filter(':even').each(function () {
+            var element = $(this);
+            element.append('<td><div class="table-expandable-arrow"></div></td>');
+        });
+    });
 
+}
 function renderAbout() {
     // filling the page with the contents of the developers collection (eventually, would become a map with markers)
     readAndFillTable($("#about"), "developers", false, false);
@@ -139,4 +158,6 @@ function readAndFillTable(page, collection, withEditBtn = true, withDeleteBtn = 
             callback(data);
         }
     });
+
+    
 }
